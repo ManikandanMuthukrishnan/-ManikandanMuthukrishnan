7be43a601a954362cf16b94d93fa99120f09ecac
Manikandan muthukrishnan <manimaverick993@gmail.com>
2019-03-30 17:44:59 +0530

    Baseclass added

---
+++ src/gitpractisesession1/Baseclass.java
@@ -0,0 +1,35 @@
+package gitpractisesession1;
+
+public class Baseclass {
+	public void greensOmr()
+  
+  {
+	  
+System.out.println("greenstech OMR branch");
+	
+	}
+	public void greensAdayar()
+	  
+	  {
+		  
+	System.out.println("greenstech Adayar branch");
+		
+		}
+	
+	public void greensTambaram()
+	  
+	  {
+		  
+	System.out.println("greenstech Tamabaram branch");
+		
+		}	
+	
+	public static void main(String[] args) {
+		Baseclass m=new Baseclass();
+		m.greensAdayar();
+		m.greensOmr();
+		m.greensTambaram();
+		
+	}
+
+}
